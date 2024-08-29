52fb1027a5230e1feaf7e5499385d87fefc6e436
kabigon23 <shaning7@gmail.com>
2024-08-30 00:28:41 +0900

    Add canvas frame for Hangman game interface

--- hangman.py
+++ hangman.py
@@ -2,9 +2,21 @@ import PySimpleGUI as sg
 
 class Hangman:
     def __init__(self) -> None:
+        layout = [
+            [
+                self._build_canvas_frame(),
+                # self._build_letters_frame(),
+            ],
+            [
+                # self._build_guessed_word_frame(),
+            ],
+            [
+                # self._build_action_buttions_frame(),
+            ],
+        ]
         self.window = sg.Window(
             title="Hangman",
-            layout=[[]],
+            layout=layout,
             finalize=True,
             margins=(100, 100),
         )
@@ -16,6 +28,22 @@ class Hangman:
     
     def close(self):
         self.window.close()
+
+    def _build_canvas_frame(self):
+        return sg.Frame(
+            "Hangman",
+            [
+                [
+                    sg.Graph(
+                        key="-CANVAS-",
+                        canvas_size=(200, 400),
+                        graph_bottom_left=(0, 0),
+                        graph_top_right=(200, 400),
+                    )
+                ]
+            ],
+            font="Any 20",
+        )
 
 if __name__ == "__main__":
     game = Hangman()
